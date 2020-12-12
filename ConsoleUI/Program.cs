f70146e8aa56ae771ffdd421a25650364c6e2031
Edward Ziemiński <edward.zieminski@gmail.com>
2020-12-12 18:01:15 +0100

    Added console program to demonstrate Kata

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,3 +1,17 @@
-﻿using System;
+﻿using KataLibrary;
+using System;
 
-Console.WriteLine();
+Console.WriteLine("KATA 003: Isograms\n");
+
+Console.Write("Give me some word: ");
+string inputWord = Console.ReadLine();
+
+bool isTheWordAnIsogram = Kata.IsIsogram(inputWord);
+
+if (isTheWordAnIsogram)
+    Console.WriteLine("\nYour word IS an isogram!");
+else
+    Console.WriteLine("\nYour word IS NOT an isogram!");
+
+Console.Write("\nPress any key...");
+Console.ReadKey();
