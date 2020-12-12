7e8c480e40fea1013a38619b4bed05c6e3ab2879
Edward Ziemiński <edward.zieminski@gmail.com>
2020-12-12 16:29:19 +0100

    Add project files.

---
+++ UnitTests/ExampleBasedUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+public class ExampleBasedUnitTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
