40fdd24e4b4d5ab4aee3171041adddcfccea7c19
Edward Ziemiński <edward.zieminski@gmail.com>
2020-12-12 16:42:52 +0100

    Added StringHelper class with IsValidWord method

--- KataLibrary/Kata.cs
+++ KataLibrary/Kata.cs
@@ -4,6 +4,17 @@ public class Kata
 {
     public static bool IsIsogram(string word)
     {
+        if (StringHelper.IsValidWord(word) == false)
+            throw new Exception("The provided word is invalid.");
+
+
+    }
+}
+
+public static class StringHelper
+{
+    public static bool IsValidWord(string word)
+    {
         throw new NotImplementedException();
     }
 }
